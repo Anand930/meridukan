983b30b10c834a887c6898f05bf52d5067d45596
Anand Sharma <anandkumarsharma93068@gmail.com>
2025-04-11 18:38:42 +0530

    spinner component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
@@ -17,33 +17,49 @@ import UpdateSellingPrice from "./pages/UpdateSellingPrice";
 import UpdateCostPrice from "./pages/UpdateCostPrice";
 import UpdateProductQuantity from "./pages/UpdateProductQuantity";
 import UpdateDescription from "./pages/UpdateDescription";
+import Spinner from "./utils/Spinner";
 
 function App() {
-  
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"/product/:id"} element={<ProductDetails />} />
-          <Route path={"/categories"} element={<Categories />} />
-          <Route path={"/profile"} element={<Profile />} />
-          <Route path={"/addcustomers"} element={<AddCustomers />} />
-          <Route path={"/addproduct"} element={<AddProduct />} />
-          <Route path={"/product"} element={<Product />} />
-          <Route path={"/login"} element={<Login />} />
-          <Route path={"/signIn"} element={<SignIn />} />
-          <Route path={"/listcustomers"} element={<ListCustomer />} />
-          <Route path={"/payamount"} element={<PayAmount/>}/>
-          <Route path={"/updateproduct"} element={<UpdateProduct/>}/>
-          <Route path={"/updateproductdetails"} element={<UpdateDataCard/>}/>
-          <Route path={"/updateproduct/updatesellingprice"} element={<UpdateSellingPrice/>}/>
-          <Route path={"/updateproduct/updatecostprice"} element={<UpdateCostPrice/>}/>
-          <Route path={"/updateproduct/updatequantity"} element={<UpdateProductQuantity/>}/>
-          <Route path={"/updateproduct/updatedescription"} element={<UpdateDescription/>}/>
-
-        </Routes>
-      </Router>
+      <Suspense fallback={<Spinner/>}>
+        <Router>
+          <Routes>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/product/:id"} element={<ProductDetails />} />
+            <Route path={"/categories"} element={<Categories />} />
+            <Route path={"/profile"} element={<Profile />} />
+            <Route path={"/addcustomers"} element={<AddCustomers />} />
+            <Route path={"/addproduct"} element={<AddProduct />} />
+            <Route path={"/product"} element={<Product />} />
+            <Route path={"/login"} element={<Login />} />
+            <Route path={"/signIn"} element={<SignIn />} />
+            <Route path={"/listcustomers"} element={<ListCustomer />} />
+            <Route path={"/payamount"} element={<PayAmount />} />
+            <Route path={"/updateproduct"} element={<UpdateProduct />} />
+            <Route
+              path={"/updateproductdetails"}
+              element={<UpdateDataCard />}
+            />
+            <Route
+              path={"/updateproduct/updatesellingprice"}
+              element={<UpdateSellingPrice />}
+            />
+            <Route
+              path={"/updateproduct/updatecostprice"}
+              element={<UpdateCostPrice />}
+            />
+            <Route
+              path={"/updateproduct/updatequantity"}
+              element={<UpdateProductQuantity />}
+            />
+            <Route
+              path={"/updateproduct/updatedescription"}
+              element={<UpdateDescription />}
+            />
+          </Routes>
+        </Router>
+      </Suspense>
     </div>
   );
 }
