e05373308560695c6993c76593c818c4453fd86e
Anand Sharma <anandkumarsharma93068@gmail.com>
2025-04-15 09:57:54 +0530

    set same site to lax which is bydefault value

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -77,7 +77,7 @@ const SignInUser = async (req, res) => {
       .cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: true,
-        sameSite:"Strict",
+        // sameSite:"Strict",
         maxAge: 7 * 24 * 60 * 60 * 1000, // ✅ 7 days in milliseconds
       })
       .json({
@@ -126,7 +126,7 @@ const LoginUser = async (req, res) => {
         .cookie("refreshToken", refreshToken, {
           httpOnly: true,
           secure: true,
-          sameSite:"Strict",
+          // sameSite:"Strict",
           maxAge: 7 * 24 * 60 * 60 * 1000, // ✅ 7 days in milliseconds
           // Which equals: 604800000
         })
