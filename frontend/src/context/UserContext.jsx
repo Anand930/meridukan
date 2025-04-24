300986f3a558448523f7e38c500dfe71e77c46d1
Anand Sharma <anandkumarsharma93068@gmail.com>
2025-04-24 17:57:36 +0530

    final commit for porfolio

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -17,7 +17,7 @@ const UserProvider = ({ children }) => {
   const handleProduct = async () => {
     try {
       const response = await fetchWithAuth(
-        "/api/product/getproduct"
+        "https://curved-jeniffer-anandsharma-521f7f2a.koyeb.app/api/product/getproduct"
       );
       setLoading(true)
       
@@ -45,7 +45,7 @@ const UserProvider = ({ children }) => {
     try {
 
       const response = await fetchWithAuth(
-        "/api/user/logout",
+        "https://curved-jeniffer-anandsharma-521f7f2a.koyeb.app/api/user/logout",
         {
           method: "POST",
           credentials: "include",
